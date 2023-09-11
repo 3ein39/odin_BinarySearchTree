7919f3786663fc2baa13010c84b7029d5f943904
HusseinHany <hhany3997@gmail.com>
2023-09-11 13:47:07 +0300

    insertion and printing

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -10,6 +10,53 @@ class Node {
 class Tree {
     constructor(arr) {
         this.root = null;
-        this.buildTree(arr);
+        this.root = this.buildTree(arr);
+    }
+
+    buildTree(arr) {
+        arr.forEach((value) => {
+            this.insert(value);
+        });
+        return this.root;
+    }
+
+    // iterative insertion of nodes
+    insert(value) {
+        if (!this.root) {
+            this.root = new Node(value);
+            return;
+        }
+        let currentNode = this.root;
+        while (currentNode) {
+            if (value < currentNode.value) {
+                if (!currentNode.left) {
+                    currentNode.left = new Node(value);
+                    break;
+                }
+                currentNode = currentNode.left;
+            } else {
+                if (!currentNode.right) {
+                    currentNode.right = new Node(value);
+                    break;
+                }
+                currentNode = currentNode.right;
+            }
+        }
     }
 }
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+const arr = [10, 5, 15, 2, 13, 22, 1, 14];
+const tree = new Tree(arr);
+prettyPrint(tree.root);
